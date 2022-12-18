a7ad8066cd77ae36c34dba091d11852de6f0a313
Krystal Zhang <72481348+KrystalZhang612@users.noreply.github.com>
2022-12-18 06:59:25 -0800

    last commit

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 export const geoApiOptions = {
     method: 'GET',
     headers: {
-        'X-RapidAPI-Key': '9f92388fbamsh2e008548e615b32p1c410ajsne12c35239c3e',
+        'X-RapidAPI-Key': 'YOUR OWN GEODB API KEY FROM RAPID API',
         'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
     }
 };
@@ -10,5 +10,4 @@ export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
 
-export const WEATHER_API_KEY = "f13d5af96be223d2a7b9d72f0a3dd2ce";
-
+export const WEATHER_API_KEY = "YOU OWN WEATHER API KEY FROM OPEN WEATHER";
