aa1e506e31c0a91961d014f3eee216912406b9e1
Sarnab Banerjee <sarnabbanerjee02@gmail.com>
2023-09-24 14:16:30 +0530

    Firat Commit

--- src/api.js
+++ src/api.js
@@ -1,13 +1,13 @@
 export const geoApiOptions = {
-    method: 'GET',
-    headers: {
-        'X-RapidAPI-Key': 'YOUR OWN GEODB API KEY FROM RAPID API',
-        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-    }
+	method: "GET",
+	headers: {
+		"X-RapidAPI-Key": "744c3eaf3fmsh277a458132a03dcp15cb97jsn805436b8ecc6",
+		"X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+	},
 };
 
-export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"; 
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
 
-export const WEATHER_API_KEY = "YOU OWN WEATHER API KEY FROM OPEN WEATHER";
+export const WEATHER_API_KEY = "90409ffa13314c260023bb438ad695a1";
